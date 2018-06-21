c42aaafab3682d0453b1200685472a3985463e04
errorr404 <dkbishwash@gmail.com>
2018-06-22 03:23:58 +0530

    Use of YTSearch library

--- src/component/container.js
+++ src/component/container.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search'
+const API_KEY= 'AIzaSyAwN3v6hMJfe1b4UnRmLZX9fwVKpSDGyfg';
+YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
+console.log(data);
+});
 class Container extends React.Component {
   render() {
     return(
--- src/component/search_bar.js
+++ src/component/search_bar.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends React.Component{
+  constructor(props) {
+    super(props);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.state = {
+      term: ''
+    };
+  }
   onInputChange(e) {
     const input= e.target.value;
-    console.log(input);
+    this.setState(()=>{
+       return {
+         term: input
+       };
+    });
+  //  console.log(input);
   }
   render() {
     return (
       <div>
-        <input onChange={this.onInputChange}/>
+        <input
+          value ={this.state.term}
+          onChange={this.onInputChange}/>
+        <p>value of state; {this.state.term}</p>
       </div>
     );
   }
