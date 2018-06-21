c8bd836d0384eb1a72c9c62cea675c881424e4db
errorr404 <dkbishwash@gmail.com>
2018-06-22 04:23:30 +0530

    fetch the videos from youtube and maintian the list

--- src/component/container.js
+++ src/component/container.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search'
+import VideoList from './video_list';
 const API_KEY= 'AIzaSyAwN3v6hMJfe1b4UnRmLZX9fwVKpSDGyfg';
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
-console.log(data);
-});
+
 class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // array for videos
+      videos: []
+    };
+  YTSearch({key:API_KEY, term: 'react'},(videos)=>{
+    // update the list of videos
+  //  console.log(videos);
+    this.setState(()=>{
+        return {
+        videos: videos
+        };
+    });
+    //this.setState ({ videos });
+    });
+  }
   render() {
     return(
       <div>
         <SearchBar />
+        <VideoList videos = {this.state.videos}/>
       </div>
     );
   }
